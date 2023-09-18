c55f1ccaa23986cc5399c4fa61c36a90e83b5290
RicardoJGBarbosa <145361706+RicardoJGBarbosa@users.noreply.github.com>
2023-09-18 15:32:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parents new healthy dinner")
 streamlit.header("Breakfast")
 streamlit.text(" 🐔 Boiled egg")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
